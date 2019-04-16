eda618ba7957467784b07193075d5feb416fce2f
GuardCat <storktec@mail.ru>
2019-04-16 20:28:33 +0300

    черновик генератора карточек

--- js/cardMaker.js
+++ js/cardMaker.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+/*jshint browser: true */
 class CardMaker {
 	constructor(table, container) {
 		let result = [];
@@ -6,9 +8,9 @@ class CardMaker {
 			for(let col in el) {
 				row = document.createElement("div");
 				h = document.createElement("b");
-				text = document.createElement("p");
+				text = document.createElement("span");
 				h.innerHTML = col + ": ";
-				text.innerHTML = el[col].toString();
+				text.innerHTML = el[col].source ? el[col].source : el[col];
 				
 				row.appendChild(h);
 				row.appendChild(text);
