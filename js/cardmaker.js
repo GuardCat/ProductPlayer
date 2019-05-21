200d8675c5647c0467586d29237bcc355900e923
Либин Николай Павлович <N.Libin@isb>
2019-05-21 13:28:52 +0300

    Исправлена генерация ссылок полисов для разных компаний (ранее для списка генерировалась битая ссылка при отсутствии полиса для СК)

--- js/cardmaker.js
+++ js/cardmaker.js
@@ -70,10 +70,12 @@ class SACard {
 			let result = "", names = entry.company.value, polices = entry.policyurl ? entry.policyurl.split(";") : "" ;
 			
 			entry.company.value.forEach( (e, i) => {
+				let policy = polices ? polices[i] : "";
+				policy = policy !== undefined ? policy : "";
 				result += `
 					<div class="description tabpad-${ i }">
 						<img src="${ e.logo }" class="companyimg">
-						<a class="policy ${ polices ? "" : "hidden" }" href="${ polices ? polices[i] : "" }">ПОЛИС</a>
+						<a class="policy ${ policy ? "" : "hidden" }" href="${ policy : "" }">ПОЛИС</a>
 						<a class="policy ${ e.instructionurl ? "" : "hidden" } ${entry.type === "SA" ? "" : "hidden"}" href="${ e.instructionurl }">ИНСТРУКЦИЯ</a>
 						<a class="policy ${entry.type === "SA" ? "" : "hidden"}" href="${ e.war }" target="blank">ОФОРМЛЕНИЕ</a>
 					</div>	
